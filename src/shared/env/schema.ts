8d6f4f79824e6a71cfaee3c4ddce0916d0f7048b
Sazzo <felipe@sazz.dev>
2025-03-16 18:24:36 -0300

    fix(env): coerce port value to number

--- src/shared/env/schema.ts
+++ src/shared/env/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
 
   KONSI_INSS_API_URL: z.string(),
   KONSI_INSS_API_AUTH_USERNAME: z.string(),
